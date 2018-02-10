7de2c8c7c2657659eb90ed7f517b2c26e64c48b6
Gray Olson <gray@grayolson.com>
2018-02-09 23:55:53 -0700

    cleanup decaf comments

--- src/js/site.js
+++ src/js/site.js
@@ -1,10 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-
 import 'babel-polyfill'
 import qr from 'qrcode-generator'
 import '../css/bootstrap.min.css'
